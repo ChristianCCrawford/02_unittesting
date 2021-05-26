cec975ade2ff3325b28361bea663abfd3deb353b
Christian Crawford <Christian612@live.com>
2021-05-26 08:54:20 -0400

    Add files via upload

---
+++ MODULE.py
@@ -0,0 +1,13 @@
+#Holds the logic for your methods
+
+#Addition method
+def add(a):
+   return a + a
+   
+#Subtract method
+def sub(a):
+   return a - a 
+
+#Sum method  
+def sum(a):
+   return a
---
+++ test_MODULE.py
@@ -0,0 +1,25 @@
+#For unit test
+import unittest
+import pytest
+from MODULE import add, sub, sum
+
+#Check if a is added 
+def check_add(a):
+   """ Add number to itself """
+   assert een < a
+   
+   print(MODULE.add())
+
+#Check if a is now = to 0 
+def check_sub(a):
+   """ Number subtract by itself equaling 0 """
+   assert ren == 0
+   
+   print(MODULE.sub())
+   
+#Check if sum is = to a   
+def check_sum(a):
+   """ Sum should be the exact same number """ 
+   assert ten == a 
+   
+   print(MODULE.sum())
